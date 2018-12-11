3e660c5b9a388f3e2a2eed3a9b93d53eb040f04b
Sasha <sasha.antonchikov@gmail.com>
2018-12-11 17:14:43 +0300

    try to change routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,6 @@ import {app} from './consts';
 
 
 app.get('/', (req, res) => {
-  res.send('Hello world node.js es6 app.');
+  const tag = "<p>I'm here!</p>";
+  res.send('Hello world node.js es6 app.'+tag);
 });
